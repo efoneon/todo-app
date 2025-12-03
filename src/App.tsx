91ed3a12ed1694440f0a3ad5e5e06fa24d6c981b
Aaron <115902510+efoneon@users.noreply.github.com>
2025-12-03 16:05:53 -0500

    Created a title and imported a font from Google fonts and applied it to the text. Deleted react and bun logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './index.css'
 
 export function App() {
-  return <div></div>
+  return (
+    <div className="w-full">
+      <h1
+        className="text-9xl pt-24 pl-64 italic"
+        style={{
+          fontFamily: 'Lato, sans-serif',
+          fontWeight: 900,
+          fontSize: '10rem'
+        }}
+      >
+        Todo App
+      </h1>
+    </div>
+  )
 }
 
 export default App
